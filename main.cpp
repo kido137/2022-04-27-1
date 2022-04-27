708037b107a9ac86e58478d2a9121f7ba3b1975f
CzDelta <kido137@naver.com>
2022-04-27 17:14:06 +0900

    2020 04 27 마무리

--- main.cpp
+++ main.cpp
@@ -10,17 +10,63 @@ int main()
 	return 0;
 }
 */
+
+
+/*
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	int Godl = 10;
+	int Godl = 0;
+
+	Godl = 100;
 
 	cout << "Hello world" << endl;
+	/*
+	cout << Godl + 100 << endl;
+	cout << Godl - 100 << endl;
+	cout << Godl * 100 << endl;
+	cout << Godl / 100 << endl;
+	// * /
+	cout << Godl % 3 << endl;
 
-	cout << Godl << endl;
+	// + - * / 
 
 	return 0;
+}
+*/
+
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float R = 0.1f;
+
+	cout << R + 0.1f << endl;
+	cout << R - 0.1f << endl;
+	cout << R * 0.1f << endl;
+	cout << R / 0.1f << endl;
+	//cout << R % 0.1f << endl;
+		
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//int float cher
+	//+ - * / %
+	char Ch = 65;  //byte
+
+	cout << (char)(Ch + 2) << endl;
+	
 }
